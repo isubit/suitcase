42a08cf7f577892f1d1db39927b2599367892c74
michael hofmockel <michael.hofmockel@gmail.com>
2014-01-29 14:36:26 -0600

    adding new suitcase template LUGG-28 LUGG-42

--- templates/region--branding.tpl.php
+++ templates/region--branding.tpl.php
@@ -10,13 +10,14 @@
       
     
     <div id="isu_wordmark">
-<a accesskey="1" class="nameplate" href="http://www.iastate.edu"><img src=http://test.ent.iastate.edu/isuitcbase/sites/all/themes/suitcase/images/sprite.png></a></div>
+<a accesskey="1" class="nameplate" href="http://www.iastate.edu"><img src="<?php DRUPAL_ROOT; ?>/sites/all/themes/suitcase/images/sprite.png"></a></div>
       <?php if ($site_name || $site_slogan): ?>
       <?php $class = $site_name_hidden && $site_slogan_hidden ? ' element-invisible' : ''; ?>
       <hgroup class="site-name-slogan<?php print $class; ?>"> 
         <?php if ($site_slogan): ?>
-        <?php $class = $site_slogan_hidden ? ' element-invisible' : ''; ?>
-        <h6 class="site-slogan<?php print $class; ?>"><a href="http://www.ent.iastate.edu"><?php print $site_slogan; ?></a></h6>
+          <?php $class = $site_slogan_hidden ? ' element-invisible' : ''; ?>
+          <h3 class="site-slogan<?php print $class; ?>"><a href="<?php print $dept_url; ?>"><?php print $site_slogan; ?></a></h3>
+        <?php endif; ?>
 
         <?php if ($site_name): ?>
         <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
@@ -24,7 +25,6 @@
         <h1 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
         <?php else: ?>
         <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
-        <?php endif; ?>
         <?php endif; ?>
         <?php endif; ?>
       </hgroup>
