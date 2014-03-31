0c511e5c613436486adbd13e753e8a63e9ab1c34
rlfrahm <frahmryan@gmail.com>
2014-03-31 16:46:46 -0500

    Bug fix

--- js/suitcase_facet.js
+++ js/suitcase_facet.js
@@ -9,9 +9,9 @@
     Drupal.behaviors.suitcaseFacet = {
         attach: function (context) {
             $('body', context).once('suitcaseFacet', function() {
-                $('.contextual-links-region .block-title').click(function() {
+                $('.block-facetapi.contextual-links-region .block-title').click(function() {
                     console.log($(this).eq(), $(this).index());
-                    $(this).parent().find('ul.facetapi-facetapi-links').slideToggle(200);
+                    $(this).parent().find('.item-list').slideToggle(200);
                 });
             });
         }
