489e383d474934e9eeec44b03a9f185a3f5c853e
rlfrahm <frahmryan@gmail.com>
2014-05-08 15:28:58 -0500

    LUGG-13 : disabled theme level breadcrumbs

--- template.php
+++ template.php
@@ -25,18 +25,18 @@ function suitcase_preprocess_region(&$vars) {
 
 // Setting breadcrumb to include current title
 // https://drupal.org/node/743366
-function suitcase_breadcrumb($variables) {
-  $breadcrumb = $variables['breadcrumb'];
-  if (!empty($breadcrumb)) {
-    // Adding the title of the current page to the breadcrumb.
-    $breadcrumb[] = drupal_get_title();
-    // Provide a navigational heading to give context for breadcrumb links to
-    // screen-reader users. Make the heading invisible with .element-invisible.
-    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
-    return $output;
-  }
-}
+//function suitcase_breadcrumb($variables) {
+//  $breadcrumb = $variables['breadcrumb'];
+//  if (!empty($breadcrumb)) {
+//    // Adding the title of the current page to the breadcrumb.
+//    $breadcrumb[] = drupal_get_title();
+//    // Provide a navigational heading to give context for breadcrumb links to
+//    // screen-reader users. Make the heading invisible with .element-invisible.
+//    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+//    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+//    return $output;
+//  }
+//}
 
 function suitcase_preprocess_block(&$vars) {
   if($vars['block']->bid === '51' || $vars['block']->module === 'facetapi') {
