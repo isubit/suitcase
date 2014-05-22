beaf4c215263d7e311613267d7d068f90d183117
rlfrahm <frahmryan@gmail.com>
2014-05-22 16:52:38 -0500

    LUGG-74 : bug fix where an error would occur if logged in as user on the edit page

--- template.php
+++ template.php
@@ -21,7 +21,7 @@ function suitcase_preprocess_html(&$vars) {
 // template_preprocess_region
 function suitcase_preprocess_region(&$vars) {
   $vars['dept_url'] = variable_get('dept_url', $default = NULL);
-  if($vars['elements']['#region'] == 'content' && arg(0) == 'node' && is_numeric(arg(1))) {
+  if($vars['elements']['#region'] == 'content' && arg(0) == 'node' && is_numeric(arg(1)) && arg(2) != 'edit') {
     $node = node_load(arg(1));
     if($node->type == 'people') {
       $vars['categories'] = array();
