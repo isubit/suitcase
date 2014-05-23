0cdba7347abf89b30a4e1a80b8e81228a96cd42c
rlfrahm <frahmryan@gmail.com>
2014-05-23 08:38:04 -0500

    LUGG-74 : Actual fix for instance where user would get an error on the view or edit page

--- template.php
+++ template.php
@@ -21,9 +21,9 @@ function suitcase_preprocess_html(&$vars) {
 // template_preprocess_region
 function suitcase_preprocess_region(&$vars) {
   $vars['dept_url'] = variable_get('dept_url', $default = NULL);
-  if($vars['elements']['#region'] == 'content' && arg(0) == 'node' && is_numeric(arg(1)) && arg(2) != 'edit') {
+  if($vars['elements']['#region'] == 'content' && arg(0) == 'node' && is_numeric(arg(1)) && arg(2) !== 'edit') {
     $node = node_load(arg(1));
-    if($node->type == 'people') {
+    if($node->type == 'people' && !empty($node->field_people_category)) {
       $vars['categories'] = array();
       foreach($node->field_people_category[LANGUAGE_NONE] as $category) {
         $tax = taxonomy_term_load($category['tid']);
