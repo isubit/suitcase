8f9afa75a2ff56aa771789ccf187a07839fbb3e0
rlfrahm <frahmryan@gmail.com>
2014-09-23 10:10:57 -0500

    Refactored facet js and tweaked css to accent

--- js/suitcase_facet.js
+++ js/suitcase_facet.js
@@ -9,78 +9,16 @@
     Drupal.behaviors.suitcaseFacet = {
         attach: function (context) {
             $('body', context).once('suitcaseFacet', function() {
-                $('.region-sidebar-first').find('.block-facetapi').addClass('js-enabled-facet');
-                $('.region-sidebar-first').find('.block-apachesolr-search').addClass('js-enabled-facet');
+                $('aside section.block').addClass('js-suitcase-facet');
 
-                var $itemList = $('.js-enabled-facet .item-list'),
-                    collapsed = 'js-enabled-collapsed',
-                    isTouchedAlready = false,
-                    dwnimg = Drupal.settings.basePath + 'sites/all/themes/suitcase/images/white-arrow-down.gif',
-                    upimg = Drupal.settings.basePath + 'sites/all/themes/suitcase/images/white-arrow-up.gif',
-                    arrowimg = 'collapsed-indicator-img',
-                    arrowclass = '.' + arrowimg;
+                $('.js-suitcase-facet .block-title').each(function() {
+                    $(this).append('<span class="caret"></span>');
+                }).bind('click touchend', function(e) {
+                    $(this).parent().find('.content').toggle();
+                });
 
                 if($(window).width() < 980) {
-                    addCollapse($itemList);
-                } else {
-                    removeCollapse($itemList);
-                }
-
-                $(window).resize(function() {
-                    if($(this).width() > 979) {
-                        removeCollapse($itemList);
-                    } else {
-                        if(!isCollapsed($itemList)) {
-                            addCollapse($itemList);
-                        }
-                    }
-                });
-
-                $('.js-enabled-facet h2.block-title').bind('click touchend', function(e) {
-                    if(!$(e.target).is('a')) {
-                        toggleCollapse($(this).parent().find('.item-list'));
-                    }
-                    if(isTouchedAlready) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                    }
-                    isTouchedAlready = (e.type == 'touchend');
-                }).bind('touchstart', function(e) {
-                    e.preventDefault();
-                });
-
-                function addCollapse($items) {
-                    $items.addClass(collapsed);
-                    $items.parent().parent().each(function() {
-                        if($(this).hasClass('block-inner clearfix') && $(this).find(arrowclass).length == 0) {
-                            $(this).find('h2').append('<img src="' + dwnimg + '" class="' + arrowimg + '">');
-                        } else if($(this).find(arrowclass).length > 0) {
-                            $(this).find(arrowclass).attr('src', dwnimg);
-                        }
-                    });
-                }
-
-                function removeCollapse($items) {
-                    $items.removeClass(collapsed);
-                    $items.parent().parent().each(function() {
-                        if($(this).hasClass('block-inner clearfix') && $(this).find(arrowclass).length == 0) {
-                            $(this).find('h2').append('<img src="' + upimg + '" class="' + arrowimg + '">');
-                        } else if($(this).find(arrowclass).length > 0) {
-                            $(this).find(arrowclass).attr('src', upimg);
-                        }
-                    });
-                }
-
-                function toggleCollapse($item) {
-                    if(isCollapsed($item)) {
-                        removeCollapse($item);
-                    } else {
-                        addCollapse($item);
-                    }
-                }
-
-                function isCollapsed($items) {
-                    return $items.hasClass(collapsed);
+                    $('.js-suitcase-facet .content').toggle();
                 }
             });
         }
