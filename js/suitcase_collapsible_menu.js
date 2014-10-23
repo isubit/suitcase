78a36956bac01d044ddd7430f9731e3cd5974a54
rlfrahm <frahmryan@gmail.com>
2014-10-23 10:39:35 -0500

    LUGG-172 : Added conditional check for more than one menu item

--- js/suitcase_collapsible_menu.js
+++ js/suitcase_collapsible_menu.js
@@ -8,16 +8,18 @@
     Drupal.behaviors.suitcaseCollapsibleMenu = {
         attach: function (context) {
             $('body', context).once('suitcaseCollapsibleMenu', function() {
-            	var li = '<li id="suitcase-main-menu-dropdown-toggle"><a>...</a></li>';
-                var touched = false;
+                if($('#main-menu > li').length > 1) {
+                    var li = '<li id="suitcase-main-menu-dropdown-toggle"><a>...</a></li>';
+                    var touched = false;
 
-                $('#main-menu > li:first').after(li);
+                    $('#main-menu > li:first').after(li);
 
-                $('#suitcase-main-menu-dropdown-toggle').bind('click touchend', function(e) {
-                    if(touched) {touched=false;return;}
-                    $('.main-menu-dropdown-item').toggleClass('hide');
-                    touched = (e.type == 'touchend');
-                }).nextAll().addClass('main-menu-dropdown-item').addClass('hide');
+                    $('#suitcase-main-menu-dropdown-toggle').bind('click touchend', function(e) {
+                        if(touched) {touched=false;return;}
+                        $('.main-menu-dropdown-item').toggleClass('hide');
+                        touched = (e.type == 'touchend');
+                    }).nextAll().addClass('main-menu-dropdown-item').addClass('hide');
+                }
             });
         }
     };
