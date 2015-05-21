f6946c43c9e99aaffc3cfe21f009f3cf178d9505
Kristi Elmore <kelmore@iastate.edu>
2015-05-21 13:35:54 -0500

    Removed Department Name from header in favor of new image

--- templates/region--branding.tpl.php
+++ templates/region--branding.tpl.php
@@ -14,11 +14,6 @@
       <?php if ($site_name || $site_slogan): ?>
         <?php $class = $site_name_hidden && $site_slogan_hidden ? ' element-invisible' : ''; ?>
         <hgroup class="site-name-slogan<?php print $class; ?>">
-          <?php if ($site_slogan): ?>
-            <?php $class = $site_slogan_hidden ? ' element-invisible' : ''; ?>
-            <h3 class="site-slogan<?php print $class; ?>"><a href="<?php print $dept_url; ?>"><?php print $site_slogan; ?></a></h3>
-          <?php endif; ?>
-
           <?php if ($site_name): ?>
             <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
             <?php if ($is_front): ?>
