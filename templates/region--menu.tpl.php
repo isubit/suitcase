8b5f4391ee2c13af9bb7a332f2d8f78bc88b42f7
rlfrahm <frahmryan@gmail.com>
2016-02-09 12:55:08 -0600

    LUGG-637

--- templates/region--menu.tpl.php
+++ templates/region--menu.tpl.php
@@ -1,13 +1,5 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-    <?php if ($site_name): ?>
-      <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
-      <?php if ($is_front): ?>
-        <h1 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
-      <?php else: ?>
-        <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
-      <?php endif; ?>
-    <?php endif; ?>
 
     <?php if ($main_menu || $secondary_menu): ?>
     <nav class="navigation">
