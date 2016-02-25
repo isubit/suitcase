8289df6be831251720154c261c4f73433941e60e
rlfrahm <frahmryan@gmail.com>
2016-02-25 11:25:19 -0600

    LUGG-441, LUGG-337 : Adding search bar to top right

---
+++ templates/region--search.tpl.php
@@ -0,0 +1,6 @@
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <div class="pull-right margin-left-10"><?php print $search_form; ?></div>
+    <div class="pull-right"><?php print $content; ?></div>
+  </div>
+</div>
