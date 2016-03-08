d1494230e6bb2b65d3c1971b7b4204338bd3c008
rlfrahm <frahmryan@gmail.com>
2016-03-08 09:47:26 -0600

    LUGG-641, LUGG-647

--- templates/region--search.tpl.php
+++ templates/region--search.tpl.php
@@ -3,7 +3,7 @@
     <div class="pull-right margin-left-10">
       <?php print $search_form; ?>
       <div class="popover">
-        <h3>Try these categories:</h3>
+        <h3>Popular searches:</h3>
         <ul class="menu">
           <?php foreach ($categories as $term): ?>
           <li><a href="<?php print taxonomy_term_uri($term)['path']; ?>"><?php print $term->name; ?></a></li>
