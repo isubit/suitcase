169d3434ef9b89164b2c33516ac8a4e21bf8e326
rlfrahm <frahmryan@gmail.com>
2016-05-16 10:12:35 -0500

    LUGG-679 : Responsive fixes for the black bar

--- suitcase_interim/templates/section--header.tpl.php
+++ suitcase_interim/templates/section--header.tpl.php
@@ -7,12 +7,12 @@
             <div class="isu-dark-ribbon" id="top-strip">
               <div class="clearfix wrapper container-12">
                 <ul class="isu-responsive-toggles wd-ribbon-menu">
-                  <li><a class="search-toggle" href="http://google.iastate.edu/">search</a></li>
+                  <li><a class="search-toggle wd-ribbon-menu-header" href="http://www.iastate.edu/">iastate.edu</a></li>
                 </ul>
                 <div class="wd-ribbon-menu-stack">
                   <ul class="isu-signons wd-ribbon-menu-group first">
                     <li>
-                      <a class="wd-ribbon-menu-header" href="#">Sign-ons</a>
+                      <a class="wd-ribbon-menu-header" href="https://www.it.iastate.edu/signons/">Sign-ons</a>
                       <ul class="wd-ribbon-menu">
                         <li class="first"><a href="https://www.iastate.edu">iastate.edu</a></li>
                         <li><a href="https://www.it.iastate.edu/signons/">ISU SignOns</a></li>
@@ -21,7 +21,7 @@
                   </ul>
                   <ul class="isu-directory-info wd-ribbon-menu-group last">
                     <li>
-                      <a class="wd-ribbon-menu-header" href="http://www.iastate.edu/contact">Directory</a>
+                      <a class="wd-ribbon-menu-header" href="http://info.iastate.edu">Directory</a>
                       <ul class="wd-ribbon-menu">
                         <li class="first"><a href="http://info.iastate.edu/">ISU Directory</a></li>
                         <li><a href="http://www.fpm.iastate.edu/maps/">Maps</a></li>
