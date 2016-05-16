746c668b7d311fb8e97c603659924923fbc5c0fd
rlfrahm <frahmryan@gmail.com>
2016-05-16 09:11:06 -0500

    LUGG-679 : Resolving some errors with the collapse plugin

--- suitcase_interim/js/suitcase_collapsible_menu.js
+++ suitcase_interim/js/suitcase_collapsible_menu.js
@@ -9,10 +9,10 @@
         attach: function (context) {
             $('body', context).once('suitcaseCollapsibleMenu', function() {
                 if($('#main-menu > li').length > 1) {
-                    var li = '<li id="suitcase-main-menu-dropdown-toggle"><a>...</a></li>';
+                    var li = '<li id="suitcase-main-menu-dropdown-toggle"><a>&#9776; Menu</a></li>';
                     var touched = false;
 
-                    $('#main-menu > li:first').after(li);
+                    $('#main-menu > li:first').before(li);
 
                     $('#suitcase-main-menu-dropdown-toggle').bind('click touchend', function(e) {
                         if(touched) {touched=false;return;}
