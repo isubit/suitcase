4bf3afd9ce6a199db3514b3098a9aa219a3a5196
Angela McMahon <angelam@iastate.edu>
2016-09-22 15:46:13 -0500

    LUGG-828 Made spacing less abysmal

--- js/suitcase_gridheight.js
+++ js/suitcase_gridheight.js
@@ -3,27 +3,26 @@
  */
 
 (function($) {
-   Drupal.behaviors.suitcaseGridHeight = {
-	   attach: function (context, settings) {
-       $('body', context).once('suitcaseGridHeight', function () {
-				 $(window).load('resize.suitcaseGridHeight', function () {					 				   
-						$($('.views-row.luggage-grid').get()).each(function () {
-                 var tallest = 0;
-								 $('.views-row.luggage-grid').each(function () {
-									 if ($(this).outerHeight() > tallest) {
-									   tallest = $(this).outerHeight();									
-									 }
-								 }).each(function () { 
-									 if ($(this).height() < tallest) {
-									   $(this).css('height', tallest);
-										 }
-									 });
-								// }
-				      }).load(function () {
-					    $(this).trigger('resize.suitcaseGridHeight');
-            });
+Drupal.behaviors.suitcaseGridHeight = {
+   attach: function (context, settings) {
+	$('body', context).once('suitcaseGridHeight', function () {
+		 $(window).load('resize.suitcaseGridHeight', function () {					 				   
+			$($('.views-row.luggage-grid').get()).each(function () {
+				var tallest = 0;
+				 $('.views-row.luggage-grid').each(function () {
+					if ($(this).outerHeight() > tallest) {
+						tallest = $(this).outerHeight();									
+						}
+					}).each(function () { 
+						if ($(this).height() < tallest) {
+							$(this).css('height', tallest);
+							}
+						});
+					}).load(function () {
+						$(this).trigger('resize.suitcaseGridHeight');
 					});
-        });		  
-		 }
-  };
+				});
+			});		  
+		}
+	};
 })(jQuery);
