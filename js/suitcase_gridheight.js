1a609ce72568d8bd6869a686c8208ea45db9cdc6
Angela McMahon <angelam@iastate.edu>
2016-09-28 10:27:13 -0500

    LUGG-828 Changed namespace issues with jquery styling

--- js/suitcase_gridheight.js
+++ js/suitcase_gridheight.js
@@ -9,12 +9,12 @@ Drupal.behaviors.suitcaseGridHeight = {
 			$(window).load(function () {
 				if (window.matchMedia('(min-width: 739px)').matches) {
 				var maxHeight = 0;
-				$('.luggage-grid,.views-row').each(function () {
+				$('.luggage-gridrow,.views-row').each(function () {
 					if ($(this).height() > maxHeight) {
 						maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();									
 					}
 					});
-				$('.luggage-grid,.views-row').height(maxHeight);
+				$('.luggage-gridrow,.views-row').height(maxHeight);
 				}
 			});
 		});		  
