e60606335ec6844b2e5a780b993cf5319fe3f190
Angela McMahon <angelam@iastate.edu>
2016-09-29 14:53:18 -0500

    LUGG-828 Made gridheight responsive for window resizing

--- js/suitcase_gridheight.js
+++ js/suitcase_gridheight.js
@@ -6,11 +6,12 @@
 Drupal.behaviors.suitcaseGridHeight = {
 	attach: function (context, settings) {
 		$('body', context).once('suitcaseGridHeight', function () {
-			$(window).load(function () {
+			$(window).bind('load resize', function () {
 				if (window.matchMedia('(min-width: 739px)').matches) {
 					var maxHeight = 0;
 					for (var i = 1; i < $('.luggage-gridrow').length; i += 2) {
 						var even = i + 1;
+						$('.views-row-' + i + ', .views-row-' + even).height('auto');
 						maxHeight = $('.views-row-' + i).outerHeight() > $('.views-row-' + even).outerHeight() ? $('.views-row-' + i).outerHeight() : $('.views-row-' + even).outerHeight();									
 						$('.views-row-' + i + ', .views-row-' + even).height(maxHeight);
 					};
