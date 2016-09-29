e45061f934bb7ddad33988e78fddc899a0410071
Angela McMahon <angelam@iastate.edu>
2016-09-29 15:17:55 -0500

    LUGG-828 Added a coupole of comments for clarity

--- js/suitcase_gridheight.js
+++ js/suitcase_gridheight.js
@@ -7,10 +7,12 @@ Drupal.behaviors.suitcaseGridHeight = {
 	attach: function (context, settings) {
 		$('body', context).once('suitcaseGridHeight', function () {
 			$(window).bind('load resize', function () {
+				// Check to make sure this isn't happening on mobile
 				if (window.matchMedia('(min-width: 739px)').matches) {
 					var maxHeight = 0;
 					for (var i = 1; i < $('.luggage-gridrow').length; i += 2) {
 						var even = i + 1;
+						// Need this to reset height for responsiveness
 						$('.views-row-' + i + ', .views-row-' + even).height('auto');
 						maxHeight = $('.views-row-' + i).outerHeight() > $('.views-row-' + even).outerHeight() ? $('.views-row-' + i).outerHeight() : $('.views-row-' + even).outerHeight();									
 						$('.views-row-' + i + ', .views-row-' + even).height(maxHeight);
