afcf4aa47f1a7d4d730e5157bf9ecfde360c12b9
Nick Booher <njbooher@gmail.com>
2016-11-05 17:18:31 -0500

    LUGG-866 Add panels layout with 2 rows of blocks

---
+++ panels/layouts/suitcase_12/suitcase_12_threecol_4_4_4_tworow_stacked/suitcase-12-threecol-4-4-4-tworow-stacked.tpl.php
@@ -0,0 +1,26 @@
+<div class="panel-display omega-grid omega-12-threecol-4-4-4" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['top']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['upper_left']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['upper_middle']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['upper_right']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['lower_left']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['lower_middle']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['lower_right']; ?></div>
+  </div>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['bottom']; ?></div>
+  </div>
+</div>
