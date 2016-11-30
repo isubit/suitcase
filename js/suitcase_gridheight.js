d1b76fc43cc0e88ee81620653a092644e0649319
Nick Booher <njbooher@gmail.com>
2016-11-30 10:40:31 -0600

    LUGG-828 Use omega’s mobile detection

--- js/suitcase_gridheight.js
+++ js/suitcase_gridheight.js
@@ -7,17 +7,19 @@
     attach: function (context, settings) {
       $('body', context).once('suitcaseGridHeight', function () {
         $(window).bind('load resize', function () {
-          // Check to make sure this isn't happening on mobile and that it isn't happening on views without the luggage-gridrow class
-          if (window.matchMedia('(min-width: 739px)').matches && $('.luggage-gridrow')[0]) {
-            var maxHeight = 0;
-            for (var i = 1; i < $('.luggage-gridrow').length; i += 2) {
-              var even = i + 1;
-              // Need this to reset height for responsiveness
-              $('.views-row-' + i + ', .views-row-' + even).height('auto');
-              maxHeight = $('.views-row-' + i).outerHeight() > $('.views-row-' + even).outerHeight() ? $('.views-row-' + i).outerHeight() : $('.views-row-' + even).outerHeight();
-              $('.views-row-' + i + ', .views-row-' + even).height(maxHeight);
-            }
-            ;
+          // Check to make sure this isn't happening on mobile
+          if (!Drupal.behaviors.hasOwnProperty('omegaMediaQueries') || Drupal.omega.getCurrentLayout() != 'mobile') {
+            // Check to make sure this isn't happening on views without the luggage-grid class
+            $($('.luggage-grid').get().reverse()).each(function () {
+              var maxHeight = 0;
+              for (var i = 1; i < $(this).find('.luggage-gridrow').length; i += 2) {
+                var even = i + 1;
+                // Need this to reset height for responsiveness
+                $(this).find('.views-row-' + i + ', .views-row-' + even).height('auto');
+                maxHeight = $(this).find('.views-row-' + i).outerHeight() > $(this).find('.views-row-' + even).outerHeight() ? $(this).find('.views-row-' + i).outerHeight() : $(this).find('.views-row-' + even).outerHeight();
+                $(this).find('.views-row-' + i + ', .views-row-' + even).height(maxHeight);
+              }
+            });
           }
         });
       });
